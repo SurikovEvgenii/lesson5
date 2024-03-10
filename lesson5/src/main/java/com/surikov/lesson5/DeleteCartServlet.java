1879ae2624635f8e4dedd472f49c1043dd21b4ee
Evgenii Surikov <surikov96@mail.ru>
2024-03-10 18:47:18 +0700

    Initial commit

---
+++ lesson5/src/main/java/com/surikov/lesson5/DeleteCartServlet.java
@@ -0,0 +1,26 @@
+package com.surikov.lesson5;
+
+import com.surikov.lesson5.service.CartService;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "deleteProductServlet", value = "/delete-product-servlet")
+public class DeleteCartServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        CartService cartService = new CartService();
+        int productId = Integer.parseInt(req.getParameter("productId"));
+        int orderId = Integer.parseInt(req.getParameter("orderId"));
+        cartService.deleteCart(productId,orderId);
+
+        resp.sendRedirect("/cart.jsp");
+
+    }
+}
