da6fcfd9bf5cf09667d0c8502a74156730e7e1c1
Evgenii Surikov <surikov96@mail.ru>
2024-03-10 18:47:01 +0700

    Add method deleteCart. Rename var

--- lesson5/src/main/java/com/surikov/lesson5/service/CartService.java
+++ lesson5/src/main/java/com/surikov/lesson5/service/CartService.java
@@ -2,8 +2,6 @@ package com.surikov.lesson5.service;
 
 import com.surikov.lesson5.DbManager;
 import com.surikov.lesson5.entity.Cart;
-import com.surikov.lesson5.entity.Product;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,10 +10,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CartService {
-    public List<Cart> getCart(int id) {
+    public List<Cart> getCart(int orderNumber) {
 
         List<Cart> cartList = new ArrayList<>();
-        String query = "SELECT products.name, products.description, products.price, products_order.count FROM products JOIN products_order ON products.id = products_order.product_id JOIN orders ON orders.id = products_order.order_id  WHERE number_order ="+id;
+        String query = "SELECT products.name, products.description, products.price, products_order.count, products_order.product_id, products_order.order_id " +
+                "FROM products JOIN products_order ON products.id = products_order.product_id " +
+                "JOIN orders ON orders.id = products_order.order_id  WHERE number_order ="+orderNumber;
 
         try (Connection connection = DbManager.createConnection();
              Statement statement = connection.createStatement()) {
@@ -24,9 +24,11 @@ public class CartService {
             while (resultSet.next()) {
                 String name = resultSet.getString(1);
                 String description = resultSet.getString(2);
-                int price = resultSet.getInt(3);
-                int count = resultSet.getInt(4);
-                cartList.add(new Cart(name, description, price, count));
+                int count = resultSet.getInt(3);
+                int price = resultSet.getInt(4);
+                int productId = resultSet.getInt(5);
+                int orderId = resultSet.getInt(6);
+                cartList.add(new Cart(name, description, price, count, productId, orderId));
             }
 
             return cartList;
@@ -35,5 +37,19 @@ public class CartService {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public void deleteCart(int productId, int orderId) {
+
+        String query = "DELETE FROM products_order WHERE product_id=" + productId +" AND order_id=" + orderId;
+
+        try (Connection connection = DbManager.createConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(query);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
